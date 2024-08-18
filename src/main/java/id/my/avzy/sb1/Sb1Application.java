2b941323d630f1afce2ac252350a1fb368b3408a
uzzzzy <a.fauzy4b@gmail.com>
2024-08-18 16:53:23 +0800

    init

---
+++ src/main/java/id/my/avzy/sb1/Sb1Application.java
@@ -0,0 +1,15 @@
+package id.my.avzy.sb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb1Application.class, args);
+
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/id/my/avzy/sb1/Sb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.avzy.sb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
