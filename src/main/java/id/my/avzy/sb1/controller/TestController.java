6c9aa33644bb688717c6c052b5e30361f6108b13
uzzzzy <a.fauzy4b@gmail.com>
2024-08-18 20:16:06 +0800

    find runs

--- src/main/java/id/my/avzy/sb1/controller/TestController.java
+++ src/main/java/id/my/avzy/sb1/controller/TestController.java
@@ -1,13 +1,36 @@
 package id.my.avzy.sb1.controller;
 
 import id.my.avzy.sb1.dto.ApiResponseData;
+import id.my.avzy.sb1.repository.TestRepository;
+import id.my.avzy.sb1.run.Run;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/test")
 public class TestController {
-    @GetMapping("/test")
+    private final TestRepository testRepository;
+
+    public TestController(TestRepository testRepository) {
+        this.testRepository = testRepository;
+    }
+
+    @GetMapping("/runs/all")
+    List<Run> findAll() {
+        return testRepository.findAll();
+    }
+
+    @GetMapping("/runs/{id}")
+    Run findById(@PathVariable Integer id) {
+        return testRepository.findById(id);
+    }
+
+    @GetMapping("/")
     public ResponseEntity<ApiResponseData<String>> test() {
         ApiResponseData<String> apiResponseData = new ApiResponseData<>();
         apiResponseData.setStatus(200);
@@ -16,7 +39,7 @@ public class TestController {
         return ResponseEntity.ok(apiResponseData);
     }
 
-    @GetMapping("/test/exception")
+    @GetMapping("/exception")
     public String testException() {
         throw new Error();
     }
---
+++ src/main/java/id/my/avzy/sb1/repository/TestRepository.java
@@ -0,0 +1,31 @@
+package id.my.avzy.sb1.repository;
+
+import id.my.avzy.sb1.run.Location;
+import id.my.avzy.sb1.run.Run;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class TestRepository {
+    private List<Run> runs = new ArrayList<Run>();
+
+    public List<Run> findAll() {
+        return runs;
+    }
+
+    public Run findById(Integer id) {
+        return runs.stream().filter(run -> run.id().equals(id)).findFirst().get();
+    }
+
+    @PostConstruct
+    private void init() {
+        runs.add(new Run(1, "Run 1", LocalDateTime.now(), LocalDateTime.now().plusHours(1), 100, Location.INDOOR));
+        runs.add(new Run(2, "Run 2", LocalDateTime.now(), LocalDateTime.now().plusHours(2), 100, Location.INDOOR));
+    }
+}
+
+
